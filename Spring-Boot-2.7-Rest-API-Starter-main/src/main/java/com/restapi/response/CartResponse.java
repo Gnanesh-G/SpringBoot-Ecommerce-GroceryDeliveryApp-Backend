ee80617d34fd6a2d8593bafa5ffcc8afb48c4d1d
GNANESH-G <gnanesh.gnanapazhani@kumaran.com>
2023-11-23 21:43:02 +0530

    Mapping correction

---
+++ Spring-Boot-2.7-Rest-API-Starter-main/src/main/java/com/restapi/response/CartResponse.java
@@ -0,0 +1,18 @@
+package com.restapi.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartResponse {
+    private Long id;
+    private Long userId;
+    private String title;
+    private Double price;
+
+}
